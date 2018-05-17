d038aa4387dc266788c12a563ad02e5afe03b07e
Niels Maier <maier.niels@gmail.com>
2018-05-17 15:34:08 +0200

    Finished a basic outline of the encoding steps.

--- codebase/encoder.py
+++ codebase/encoder.py
@@ -4,6 +4,7 @@
 import numpy as np
 from scipy.io import wavfile
 from scipy import fftpack 
+from scipy import stats
 import matplotlib.pyplot as plt
 
 # Parameters
@@ -14,7 +15,18 @@ bl_size = 128
 fs, data = wavfile.read(path)
 data = np.ravel(data)
 
+# divide data in blocks
 l_over = len(data)%bl_size
 rest = data[len(data)-l_over :]
 data = data[: len(data)-l_over]
 blocks = [data[i:i+bl_size] for i in range(0, len(data), bl_size/2)]
+
+# transform blocks with DTC and Quantize them
+for b in blocks:
+	b = fftpack.dct(b, norm='ortho')
+	b = stats.threshold(b, threshmin=10)
+	b = b/16
+	b = b + 0.5
+	b = b.astype(int)
+	b = np.clip(b, 0, 256)
+
